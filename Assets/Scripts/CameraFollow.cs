6d78cbab79dd5d51efb83bd701679a737869f364
chenzh33 <chenzhenghao33@foxmail.com>
2020-07-24 21:59:34 +0800

    Modify CameraFollow, add empty file for VirtualInputManager

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    Transform playerTransform;
+	private Vector3 targetPos;
+    public float moveSpeed;
+
+    void Start () {
+
+    }
+
+    void Update () {
+        if (playerTransform != null) {
+            targetPos = new Vector3 (playerTransform.position.x, playerTransform.position.y, playerTransform.position.z);
+            Vector3 velocity = (targetPos - transform.position) * moveSpeed;
+            transform.position = Vector3.SmoothDamp (transform.position, targetPos, ref velocity, 1.0f, Time.deltaTime);
+        }
+    }
+}
