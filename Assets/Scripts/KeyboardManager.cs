7cf863702cd78524db0e671055234fd0c19dd052
chenzh33 <chenzhenghao33@foxmail.com>
2020-07-27 01:11:24 +0800

    modify VirtualInputManager, add KeyboardManager, need test in Unity

---
+++ Assets/Scripts/KeyboardManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class KeyboardManager : MonoBehaviour {
+    private InputsDataPerFrame inputs;
+   
+    void Update()
+    {
+        inputs.InputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Horizontal"));
+        inputs.InputVector = Vector3.ClampMagnitude(inputs.InputVector, 1);
+        inputs.MAttackET = Input.GetKeyDown(KeyCode.I);
+        inputs.MAttackEX = Input.GetKeyUp(KeyCode.I);
+        inputs.RAttackET = Input.GetKeyDown(KeyCode.J);
+        inputs.RAttackEX = Input.GetKeyUp(KeyCode.J);
+        inputs.DodgeET = Input.GetKeyDown(KeyCode.K);
+        inputs.DodgeEX = Input.GetKeyUp(KeyCode.K);
+        inputs.ChargeET = Input.GetKeyDown(KeyCode.L);
+        inputs.ChargeEX = Input.GetKeyUp(KeyCode.L);
+        
+        VirtualInputManager.Instance.LoadInput(inputs);
+    }
+  
+}
--- Assets/Scripts/VirtualInputManager.cs
+++ Assets/Scripts/VirtualInputManager.cs
@@ -2,13 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public struct InputsDataPerFrame{
+    public Vector2 InputVector;
+    public bool MAttackET;
+    public bool MAttackEX;
+    public bool RAttackET;
+    public bool RAttackEX;
+    public bool DodgeET;
+    public bool DodgeEX;
+    public bool ChargeET;
+    public bool ChargeEX;
+}
+
 public class VirtualInputManager : MonoBehaviour {
+    public static VirtualInputManager Instance = null;
+    private const int INPUT_BUFFER_SIZE = 16;
+    private int curIndex = 0;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else{
+            Destroy(this.gameObject);
+        }
+    }
+    private void PushInput()
+    {
+        inputBuffer[curIndex + 1] = inputBuffer[0];
+        Debug.Log(inputBuffer[curIndex + 1]);
+        curIndex = (curIndex + 1) % INPUT_BUFFER_SIZE;
+
+    }
+    void FixedUpdate()
+    {
+        PushInput();
+
+    }
+    
+    public InputsDataPerFrame GetInput()
+    {
+        return inputBuffer[curIndex + 1];
+
+    }
+
+    public void LoadInput(InputsDataPerFrame data)
+    {
+        inputBuffer[0] = data;
+    }
    
-    void Start () {
+    private InputsDataPerFrame[] inputBuffer = new InputsDataPerFrame[INPUT_BUFFER_SIZE + 1];
 
-    }
-
-    void Update () {
-      
-    }
 }
